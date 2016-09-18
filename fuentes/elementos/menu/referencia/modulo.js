e0eb2d87777506411aa18219ff9710c3f034b89a
Guillermo Carrillo <gach87@gmail.com>
2016-09-18 11:48:17 -0400

    Se implementa servicio de menu utilizando service

--- fuentes/elementos/menu/referencia/modulo.js
+++ fuentes/elementos/menu/referencia/modulo.js
@@ -1,73 +1,75 @@
 (function () {
-    angular.module("LaColaFeliz", []).directive("menu", function () {
+    angular.module("LaColaFeliz", []).directive("menu", ['servicioDeMenu', function () {
         return {
             bindToController: true,
             controllerAs: 'ctrl',
-            controller: function () {
-                var menu = [
-                    {
-                        nombre: 'Hamburguesas',
-                        tipos: [
-                            {
-                                nombre: 'Normal',
-                                precio: 2500
-                            },
-                            {
-                                nombre: 'Especial',
-                                precio: 3500
-                            },
-                            {
-                                nombre: 'Full Equipo',
-                                precio: 4000
-                            }
-                        ],
-                        activo:true
-                    }, {
-                        nombre: 'Perros Calientes',
-                        tipos: [
-                            {
-                                nombre: 'Normal',
-                                precio: 1500
-                            },
-                            {
-                                nombre: 'Especial',
-                                precio: 2000
-                            },
-                            {
-                                nombre: 'Full Equipo',
-                                precio: 2500
-                            }
-                        ],
-                        activo:false
-                    }, {
-                        nombre: 'Bebidas',
-                        tipos: [
-                            {
-                                nombre: 'Normal',
-                                precio: 800
-                            },
-                            {
-                                nombre: 'Mediano',
-                                precio: 1200
-                            },
-                            {
-                                nombre: 'Grande',
-                                precio: 1600
-                            }
-                        ],
-                        activo:true
-                    },
-                ]
-
-                this.obtenerEspecialidades = function () {
-                    return menu.filter(function(especialidad){
-                        return especialidad.activo
-                    });
-                }                
+            controller: function (servicioDeMenu) {
+                this.obtenerEspecialidades = servicioDeMenu.obtenerEspecialidades;
             },
             restrict: 'E',
             scope: {},
             templateUrl: 'elementos/menu/referencia/plantilla.html'
+        }
+    }]).service('servicioDeMenu', function () {
+        var menu = [
+            {
+                nombre: 'Hamburguesas',
+                tipos: [
+                    {
+                        nombre: 'Normal',
+                        precio: 2500
+                    },
+                    {
+                        nombre: 'Especial',
+                        precio: 3500
+                    },
+                    {
+                        nombre: 'Full Equipo',
+                        precio: 4000
+                    }
+                ],
+                activo: true
+            }, {
+                nombre: 'Perros Calientes',
+                tipos: [
+                    {
+                        nombre: 'Normal',
+                        precio: 1500
+                    },
+                    {
+                        nombre: 'Especial',
+                        precio: 2000
+                    },
+                    {
+                        nombre: 'Full Equipo',
+                        precio: 2500
+                    }
+                ],
+                activo: false
+            }, {
+                nombre: 'Bebidas',
+                tipos: [
+                    {
+                        nombre: 'Normal',
+                        precio: 800
+                    },
+                    {
+                        nombre: 'Mediano',
+                        precio: 1200
+                    },
+                    {
+                        nombre: 'Grande',
+                        precio: 1600
+                    }
+                ],
+                activo: true
+            },
+        ]
+
+        this.obtenerEspecialidades = function () {
+            return menu.filter(function (especialidad) {
+                return especialidad.activo
+            });
         }
     });
 })();
