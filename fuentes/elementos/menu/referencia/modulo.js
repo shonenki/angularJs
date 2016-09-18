614c7a839b4c570ac1806bec13d0d6d711c9d756
Guillermo Carrillo <gach87@gmail.com>
2016-09-18 12:10:42 -0400

    Se realiza la separación del menu en archivo json

--- fuentes/elementos/menu/referencia/modulo.js
+++ fuentes/elementos/menu/referencia/modulo.js
@@ -10,61 +10,15 @@
             scope: {},
             templateUrl: 'elementos/menu/referencia/plantilla.html'
         }
-    }]).factory('servicioDeMenu', function () {
-        var menu = [
-            {
-                nombre: 'Hamburguesas',
-                tipos: [
-                    {
-                        nombre: 'Normal',
-                        precio: 2500
-                    },
-                    {
-                        nombre: 'Especial',
-                        precio: 3500
-                    },
-                    {
-                        nombre: 'Full Equipo',
-                        precio: 4000
-                    }
-                ],
-                activo: true
-            }, {
-                nombre: 'Perros Calientes',
-                tipos: [
-                    {
-                        nombre: 'Normal',
-                        precio: 1500
-                    },
-                    {
-                        nombre: 'Especial',
-                        precio: 2000
-                    },
-                    {
-                        nombre: 'Full Equipo',
-                        precio: 2500
-                    }
-                ],
-                activo: false
-            }, {
-                nombre: 'Bebidas',
-                tipos: [
-                    {
-                        nombre: 'Normal',
-                        precio: 800
-                    },
-                    {
-                        nombre: 'Mediano',
-                        precio: 1200
-                    },
-                    {
-                        nombre: 'Grande',
-                        precio: 1600
-                    }
-                ],
-                activo: true
-            },
-        ]
+    }]).factory('servicioDeMenu', ['$http', function ($http) {
+
+        var menu;
+
+        $http({ method: "GET", url: "menu.json" }).then(function (dataHttp) {
+            menu = dataHttp.data;
+        }, function () {
+            new Error('Archivo json de menu no encontrado')
+        })
 
         function obtenerEspecialidades() {
             return menu.filter(function (especialidad) {
@@ -75,5 +29,5 @@
         return {
             obtenerEspecialidades: obtenerEspecialidades
         }
-    });
+    }]);
 })();
