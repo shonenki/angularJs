37d12dd628e2068c788f86c8140d431fe4b3510e
shonenki@gmail.com <shonenki@gmail.com>
2016-09-21 14:52:49 -0400

    Menu con array

--- fuentes/elementos/menu/jorge/modulo.js
+++ fuentes/elementos/menu/jorge/modulo.js
@@ -5,9 +5,87 @@
       
     ]).directive('menu', function(){ //3
         return{
+            BindtoController: true,
+            controllerAs: 'ctrl',
+            controller: function(){
+                //Se define lógica de negocios en este ejemplo para entender el funcionamiento, pero en realidad debe ir en un servicio
+                //Que es llamado desde el controladorw
+                var menu = [
+                    {
+                        nombre: 'Hamburguesas',
+                        tipos: [
+                            {
+                                nombre: 'Normal',
+                                precio: 2500
+                            },
+                            {
+                                nombre: 'Especial',
+                                precio: 3500
+                            },
+                            {
+                                nombre: 'Full Equipo',
+                                precio: 4000
+                            }
+                        ],
+                        activo:true
+                    }, {
+                        nombre: 'Perros Calientes',
+                        tipos: [
+                            {
+                                nombre: 'Normal',
+                                precio: 1500
+                            },
+                            {
+                                nombre: 'Especial',
+                                precio: 2000
+                            },
+                            {
+                                nombre: 'Full Equipo',
+                                precio: 2500
+                            }
+                        ],
+                        activo:false
+                    }, {
+                        nombre: 'Bebidas',
+                        tipos: [
+                            {
+                                nombre: 'Normal',
+                                precio: 800
+                            },
+                            {
+                                nombre: 'Mediano',
+                                precio: 1200
+                            },
+                            {
+                                nombre: 'Grande',
+                                precio: 1600
+                            }
+                        ],
+                        activo:true
+                    },
+                ]
+
+                this.obtenerEspecialidades = function () {
+                    //También es posible usar menu.filter que hace lo mismo que tengo acá a través de JS (investigar)
+                    var arr_menu = [];
+                    for (i = 0; i < menu.length; i++) { 
+                        if(menu[i].activo == true){
+                            arr_menu[i] = menu[i];
+                        }
+                    }
+                    return arr_menu;
+                }  
+            },
             restrict: 'E', //4
-            templateUrl: 'elementos/menu/jorge/plantilla.html'
-        }
+            templateUrl: 'elementos/menu/jorge/plantilla.html',
+            scope: {} //6
+            //controller: function($scopre){
+                //5
+                //$scope.menu = {
+
+                //}
+
+            }
     });
     
 })(angular);
@@ -17,6 +95,16 @@
 //2. Siempre se debe declarar obligatoriamente al menos un módulo
 //3. Se normaliza el nombre de la directiva en camelCasse porque no es posible colocarlo así en HTML
 //4. Evita que busque los elementos, es importante para ayudar al performance de la app (E, A, C, M)
+
+//*** 5 ****
+//En el Scope guardo elementos de JS y puedo embeberlos en HTML
+//Al definir un ng-app se crea un Scope root, al usar un controlador se hereda ese scope
+//ng-repeat cumple la función de un foreach
+//El ng-repeat sólo lee objetos en JS
+
+//*** 6 ***
+//BindtoController, scope {} (vacio) y ControllerAs: true. De esta manera aislo el scope padre y creo un scope exclusivo para la misma directiva
+//De esta forma se amplia el poliformismo, debido a que es posible crear los elementos dentro de la directiva sin tener ningún tipo de dependencia externa
 
 
 //*** Notas: ***
@@ -31,3 +119,12 @@
 
 //El módulo inicial se llama desde el ng-app, desde ahí se pueden referenciar el resto de los módulos 
 //Poliformismo como práctica, poder reorganizar componentes llamando uno u otros según lo necesites. Ej: src="mensaje.js" o src="mensaje2.js" teniendo ambos el mismo contenido pero con funcionabiliad distinta.
+//El Ngmodel es un two way databine (puedo busca referencias en W3C)
+//El scope es lo que permite enlazar el JS con HTML a través de Angular.
+//El ng-app va siempre pegado a un módulo, tanto las directivas como los controladores van a un módulo, por ende es necesario tener siempre al menos un módulo.
+//Estado Global NO!
+//Modelo: Vista Modelo / Modelo Vista (investigar)
+//ngif ejecuta algo en base a una condición booleana (ver ejemplo de la práctica2 del repositorio)
+//menu.filter (función de JavaScript para permitir filtrar elementos de un arreglo)
+//Definir fronteras es la finalidad de la Arquitectura de Software
+// Directivas - Controladores - Servicios
